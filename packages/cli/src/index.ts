1898884bd1a4c2f98ae6caf0f8d55f6d7b50f316
kushuri12 <kyoha199@gmail.com>
2026-04-21 09:45:24 +0800

    chore: release v1.2.4 with professional minimalist docs

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -23,7 +23,7 @@ import { SimpleTUI } from "./ui/SimpleTUI.js";
 
 import ora from "ora";
 
-export const version_cli = "1.2.3";
+export const version_cli = "1.2.4";
 
 async function main() {
   await ensureHiruDirs();
