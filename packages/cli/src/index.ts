3108d401468b5ab424c4d2fff97a53e4ae138111
kushuri12 <kyoha199@gmail.com>
2026-04-21 10:44:38 +0800

    fix: resolve version sync bug in SimpleTUI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -23,7 +23,7 @@ import { SimpleTUI } from "./ui/SimpleTUI.js";
 
 import ora from "ora";
 
-export const version_cli = "1.2.6";
+export const version_cli = "1.2.7";
 
 async function main() {
   await ensureHiruDirs();
