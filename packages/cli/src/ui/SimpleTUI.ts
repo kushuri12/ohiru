9ef9b00467aae8484a9e9f09e5b0ce5aceea229e
kushuri12 <kyoha199@gmail.com>
2026-04-21 13:04:43 +0800

    release: v1.3.5 - unchained agent, smart filtering disabled for maximum autonomy

--- packages/cli/src/ui/SimpleTUI.ts
+++ packages/cli/src/ui/SimpleTUI.ts
@@ -66,7 +66,7 @@ export class SimpleTUI {
 
   private currentVersion: string;
 
-  constructor(config: HiruConfig, version: string = "1.3.3") {
+  constructor(config: HiruConfig, version: string = "1.3.5") {
     this.config = config;
     this.currentVersion = version;
     this.provider = config.provider;
