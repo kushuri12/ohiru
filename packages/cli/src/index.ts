e7a52bd12bf5bba3a5d8f81f924f75ffeaa14b5c
kushuri12 <kyoha199@gmail.com>
2026-04-22 11:18:22 +0800

    chore: update hardcoded cli version

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -23,7 +23,7 @@ import { SimpleTUI } from "./ui/SimpleTUI.js";
 
 import ora from "ora";
 
-export const version_cli = "1.4.9";
+export const version_cli = "1.5.3";
 
 async function main() {
   await ensureHiruDirs();
